89e04674779296f4b342f42b0010d487ea7fc1e6
yizharGoldstein <izhargo@gmail.com>
2020-04-02 22:48:54 +0300

    basic grow-cherry-service.py

--- src/grow-cherry-service.py
+++ src/grow-cherry-service.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Everybody freeze, this is a robbery"}
+
+@app.post("/auth_shopper")
+async def auth_shopper(username:str, password:str):
+    pass
+
+
+@app.post("/auth_courier")
+async def auth_courier(username:str, password:str):
+    pass
+
+
+@app.post("/get_products")
+async def get_products():
+    pass
+
+
